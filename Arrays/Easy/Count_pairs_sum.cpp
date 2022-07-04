7a5447e5cc3adae659847740be77b27588af080e
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-04 21:17:11 +0530

    4-07-2022

---
+++ Arrays/Easy/Count_pairs_sum.cpp
@@ -0,0 +1,21 @@
+class solution
+{
+public:
+    int getPairsCount(int arr[], int n, int k)
+    {
+        // code here
+        unordered_map<int, int> m;
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            auto it = m.find(k - arr[i]);
+            if (it != m.end())
+            {
+                cnt += m[k - arr[i]];
+
+            }
+            m[arr[i]]++;
+        }
+        return cnt;
+    }
+};
